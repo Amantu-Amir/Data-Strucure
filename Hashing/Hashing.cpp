cd113f76f8f9fe2fcec08d9b7433efa16be772a8
Amir Hosen <42311923+amir-hosen7@users.noreply.github.com>
2023-07-15 11:28:06 +0600

    Update Hashing.cpp

--- Hashing/Hashing.cpp
+++ Hashing/Hashing.cpp
@@ -25,33 +25,34 @@ typedef tree<ll, null_type,less_equal<ll>, rb_tree_tag,tree_order_statistics_nod
 //------------------------------------------------------------------------------//
 
 
-const ll P=661;
-const ll N=1e5+50;
-const ll mod=1e9+7;
+const ll P = 661;
+const ll N = 1e5+50;
+const ll mod = 1e9+7;
 
 ll basePow[N];
 
-vector<ll> rollingHash(string &s){
-    ll siz=s.size();
-    vector<ll> H(siz+5); H[0]=0;
-    for(ll i=1; i<=siz; i++){
-        ll id=s[i-1]-'a'+1;
-        if(s[i-1]>='A' and s[i-1]<='Z'){
-            id+=26;
+vector<ll> rollingHash(string &s) {
+    ll siz = s.size();
+    vector<ll> hash(siz + 5); 
+    hash[0] = 0;
+    for(ll i = 1; i <= siz; i++) {
+        ll id = s[i - 1] - 'a' + 1;
+        if(s[i - 1] >= 'A' and s[i - 1] <= 'Z') {
+            id += 26;
         }
-        H[i]=((H[i-1]*P)%mod+id)%mod;
+        hash[i] = ((hash[i - 1] * P) % mod + id) % mod;
     }
-    return H;
+    return hash;
 }
 
-ll computeHash(string &s){
-    ll siz=s.size(), hash=0;
-    for(ll i=0; i<siz; i++){
-        ll id=s[i]-'a'+1;
-        if(s[i]>='A' and s[i]<='Z'){
-            id+=26;
+ll computeHash(string &s) {
+    ll siz = s.size(), hash = 0;
+    for(ll i = 0; i < siz; i++) {
+        ll id = s[i] - 'a' + 1;
+        if(s[i] >= 'A' and s[i] <= 'Z') {
+            id += 26;
         }
-        hash=((hash*P)%mod+id)%mod;
+        hash = ((hash * P) % mod + id) % mod;
     }
     return hash;
 }
@@ -62,34 +63,37 @@ int main(){
     //freopen("output.txt", "w", stdout);
     //ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
-    basePow[0]=1;
-    for(ll i=1; i<N; i++){
-        basePow[i]=(basePow[i-1]*P)%mod;
+    basePow[0] = 1;
+    for(ll i = 1; i < N; i++) {
+        basePow[i] = (basePow[i - 1] * P) % mod;
     }
 
-    ll t=1;
-    for(ll T=1; T<=t; T++){
+    ll t = 1;
+    for(ll T = 1; T <= t; T++) {
         string text, pat;
         getline(cin, text);
         getline(cin, pat);
-        vector<ll> textHash=rollingHash(text);
-        ll patHash=computeHash(pat);
-        ll textLen=text.size(), patLen=pat.size();
+
+        vector<ll> textHash = rollingHash(text);
+        ll patHash = computeHash(pat);
+
+        ll textLen = text.size(), patLen = pat.size();
         vector<ll> res;
-        for(ll i=1; (i+patLen-1)<=textLen; i++){
-            ll currHash=(textHash[i+patLen-1]-(textHash[i-1]*basePow[patLen])%mod+mod)%mod;
-            if(currHash==patHash){
-                res.pb(i-1);
+
+        for(ll i = 1; (i + patLen - 1) <= textLen; i++) {
+            ll currHash = (textHash[i + patLen - 1] - (textHash[i - 1] * basePow[patLen]) % mod + mod) % mod;
+            if(currHash == patHash) {
+                res.pb(i - 1);
             }
         }
-        for(auto i:res){
-            cout<<i<<" ";
+
+        for(auto i : res) {
+            cout << i << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
     return 0;
 }
-
 
 
 
